3af710edab30ef19e07237d10f720892228961bb
MIHAI POPESCU <mihai.ppscu@gmail.com>
2017-05-22 22:39:55 +0300

    add new content

--- src/homepage.template.js
+++ src/homepage.template.js
@@ -1,23 +1,25 @@
+import { issues } from './issues';
+
+let listItems = '';
+issues
+    .map(res => listItems += `<li class="f6 f5-ns b dib ph5 pv4 mr3 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-40 fw4">${res.issue}</li>`);
+
 export const homepage = `
- <div class="vh-100 dt w-100 tc bg-dark-gray white cover relative" style="background:  url(https://media.giphy.com/media/2pJfGles06wOk/giphy.gif) no-repeat center center fixed; ">
+<div class="vh-100 dt w-100 tc bg-dark-gray white cover relative" style="background:  url(https://media.giphy.com/media/2pJfGles06wOk/giphy.gif) no-repeat center center fixed; ">
         <div class="absolute absolute--fill t0 b0 h-100 w-100 bg-black-50 z-0"></div>
-        <section class="mw5 mw9-ns center pa3 ph5-ns z-1 relative">
-            <h1 class="tc f-headline lh-title display ttu fw5 white measure-narrow">What keeps you up at night ?</h1>
-            <ul class="list flex flex-wrap justify-center align-center items-center ph3 ph5-ns pv4 js-problem-list">
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Politics</li>
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Environment</li>
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Fake News</li>
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Information Overload</li>
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Privacy</li>
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Unhandled Corruption</li>
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Discrimination</li>
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Access to Internet</li>
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Human Rights</li>
-                <li class="f6 f5-ns b dib ph4 pv3 mr2 mb3 hover-black hover-bg-white bg-animate no-underline white ba br-pill b--white-20 fw4">Open Data</li>
+        <section class="mw5 mw9-ns center pa3 z-1 relative">
+            <h1 class="tc f-headline lh-title display ttu fw5 white measure-narrow mb2">What keeps you up at night ?</h1>
+            <p class="tc f4 lh-copy measure-wide fw3 center">
+                 Alege din lista de mai jos care este problema care te frământa cel mai tare. Ce te enerveaza cel mai tare in țara asta.
+                Care este problema care te ține treaz noapte de noapte. Ce ai rezolva prima data dacă ai avea puterea sa
+                o faci chiar acum.
+            </p>
+            <ul class="list flex flex-wrap justify-center align-center items-center ph3 ph5-ns pv4 mt5 js-problem-list">
+                ${listItems}
             </ul>
             <div class="mt6 tc">
                 <a rel="nofollow" rel="noreferrer" class="f3 hover-black hover-bg-white bg-animate no-underline br2 ba ph5 pv3 mb2 dib white js-submit-items"
-                    href="#0">Tell us</a>
+                    href="#0">Spune-ne</a>
             </div>
         </section>
     </div>
